c6a2bb4c0c53c36b5637914674c263551a9a9b0e
Yaroslav Klimas <yarklim@gmail.com>
2023-01-20 17:52:02 +0200

    added solution task #1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+// Прослушивание и делегирование события / открытие модального окна
+galleryListEl.addEventListener('click', openModalImg);
+
+function openModalImg(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instanceImg = basicLightbox.create(`
+        <img src="${e.target.dataset.source}" width="800" height="600">
+    `);
+
+  instanceImg.show();
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      instanceImg.close();
+    }
+  });
+}
+
+// Создаю разметку галлереи в HTML
+const imgMarcup = createItemImg(galleryItems);
+galleryListEl.insertAdjacentHTML('beforeend', imgMarcup);
+
+// Функция для создания разметки изображения
+function createItemImg(items) {
+  const imgItem = items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" onclick=“return false”>
+    <img
+      class="gallery__image"
+      src='${preview}'
+      data-source='${original}'
+      alt='${description}'
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+
+  return imgItem;
+}
